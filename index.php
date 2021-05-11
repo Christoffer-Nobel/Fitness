757fd86e3325477237c6f6869b5930192c57b412
abildgaard06 <abildgaard06@hotmail.com>
2021-05-11 09:52:38 +0200

    nyt med nyt på

--- index.php
+++ index.php
@@ -1,22 +1,11 @@
 <!DOCTYPE html>
 <?php
-//Definer konstanter med databaseforbindelse info
-define("DBHOST", "localhost");
-define("DBUSER", "root");
-define("DBPASS", "root");
-define("DBNAME", "fitness");
-
-//inkluderer funkntions.php med alle funktioner
-include("functions.php");
-
-// Forbinder til databasen (se functions.php)
-connect();
-
+include('nav.php');
 ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Fisnes - værdens feæste fissenes</title>
+    <title>Fissenes - værdens feæste fissenes</title>
   </head>
   <body>
 
--- nav.php
+++ nav.php
@@ -3,3 +3,18 @@
 - INDSÆT NYE MEMBERS
 - SE CENTER INKL. EQUIPMENT
 - EVT. INDSÆT NYE CENTRE OG EQUIPMENT
+
+<?php
+//Definer konstanter med databaseforbindelse info
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+define("DBPASS", "root");
+define("DBNAME", "fitness");
+
+//inkluderer funkntions.php med alle funktioner
+include("functions.php");
+
+// Forbinder til databasen (se functions.php)
+connect();
+
+?>
