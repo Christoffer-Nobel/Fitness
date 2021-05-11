1d1e7ba3cf79a87ef537de6a869aefc775ae3a96
Christoffer-Nobel <christoffernobel1708@gmail.com>
2021-05-11 10:26:36 +0200

    det virker ikke men er forhåbentlig et skridt på vejen

--- functions.php
+++ functions.php
@@ -12,3 +12,38 @@ function connect(){
   die(mysqli_error($conn));
 }
 }
+
+/*function get_fa(){
+
+global $conn;
+
+$sql = "SELECT fac_name FROM facility WHERE fac_id > 0";
+
+$result = mysqli_query($conn, $sql);
+
+$product = [];
+if(mysqli_num_rows($result) > 0){
+  while($row = mysqli_fetch_assoc($result)){
+    $product[] = $row;
+  }
+  }
+return $product;
+}
+*/
+function get_fac(){
+
+global $conn;
+
+$sql =  "SELECT fac_ id, fac_name FROM facility WHERE fac_id > 0";
+
+$result = mysqli_query($conn, $sql);
+
+$products = [];
+if(mysqli_num_rows($result) > 0){
+  while($row = mysqli_fetch_assoc($result)){
+
+    $products[] = $row;
+  }
+  }
+return $products;
+}
