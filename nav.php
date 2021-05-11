40aa060776de796961ceea3220da302f9cc57536
MathiasJN <73479387+MathiasJN@users.noreply.github.com>
2021-05-11 09:51:38 +0200

    Yes

---
+++ nav.php
@@ -0,0 +1,29 @@
+<nav>
+<ul id="menu">
+  <li><a href="#">OLED</a>
+    <ul>
+        <li><a href="#">65OLED855</a></li>
+        <li><a href="#">65OLED854</a></li>
+        <li><a href="#">65AG8</a></li>
+        <li><a href="#">65AG9</a></li>
+        <li><a href="#">65C8</a></li>
+        <li><a href="#">65C9</a></li>
+        <li><a href="#">65RX</a></li>
+    </ul>
+  </li>
+  <li><a href="#">QLED</a>
+    <ul>
+      <li><a href="#">QE65Q95T</a></li>
+      <li><a href="#">QE85Q95T</a></li>
+    </ul>
+  </li>
+  <li><a href="#">LCD</a>
+    <ul>
+      <li><a href="#">82TU8005</a></li>
+      <li><a href="#">65PUS8545</a></li>
+      <li><a href="#">75UN8500</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Pc-skærme</a></li>
+</ul>
+</nav>
