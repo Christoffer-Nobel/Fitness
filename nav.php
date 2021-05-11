ac9fb08847f49f9c19be2dbb796f48f770315858
kajkage <73479384+kajkage@users.noreply.github.com>
2021-05-11 10:15:02 +0200

    Update nav.php
    xD

--- nav.php
+++ nav.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
 
-- SE MEMEBERS MED ROLLER
-- INDSÆT NYE MEMBERS
-- SE CENTER INKL. EQUIPMENT
-- EVT. INDSÆT NYE CENTRE OG EQUIPMENT
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <div class="topnav">
+    <a class="active" href="members.php">Members</a>
+    <a href="newmembers.php">New members</a>
+    <a href="Facilities.php"> Facilities </a>
+    <a href="Equipment.php">Equipment </a>
+
+  </div>
+  </body>
+</html>
 
 <?php
 //Definer konstanter med databaseforbindelse info
